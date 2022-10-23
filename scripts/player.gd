1da0c416816362869b2feffaf48e20dcbd9488bf
Improjects <62845931+Piratefoxy216@users.noreply.github.com>
2022-10-22 20:16:36 -0700

    A more stable player script
    Added jump animation
    
    Added jump
    
    -Sunkist

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 var vect = Vector2(0,0)
+var up = Vector2(0,-1)
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -13,16 +14,26 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+		
 	if Input.is_action_pressed('d'):
-		vect.x = 150
-		$Player.set_flip_h(false)
-		$AnimationPlayer.set_current_animation('walk')
+		vect.x = 200
+		$AnimatedSprite.set_flip_h(false)
+		$AnimatedSprite.offset = Vector2(0,0)
+		$AnimatedSprite.set_animation('walk')
 	elif Input.is_action_pressed('a'):
-		vect.x = -150
-		$Player.set_flip_h(true)
-		$AnimationPlayer.set_current_animation('walk')
+		vect.x = -200
+		$AnimatedSprite.set_flip_h(true)
+		$AnimatedSprite.offset = Vector2(5,0)
+		$AnimatedSprite.set_animation('walk')
 	else:
 		vect.x = 0
-		$AnimationPlayer.set_current_animation('idle')
-	move_and_slide(vect)
+		$AnimatedSprite.set_animation('idle')
+	if Input.is_action_pressed('w') and is_on_floor() == true:
+		vect.y = -300
+	elif is_on_floor() == false:
+		vect.y += 10
+		$AnimatedSprite.set_animation('jump')
+		
+	move_and_slide(vect, up)
+	
