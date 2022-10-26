fdea97722c98919428d57d365b574eb1da8513ad
ChippedObelisk <josephadams122@hotmail.com>
2022-10-26 14:59:49 -0400

    Menu and Resolution Changes

--- scripts/NewGame.gd
+++ scripts/NewGame.gd
@@ -9,3 +9,7 @@ func _on_NewGame_mouse_entered():
 
 func _on_NewGame_mouse_exited():
 	set_modulate(mouse_exited)
+
+
+func _on_NewGame_pressed():
+	.get_tree().change_scene("res://scenes/test/PlayerTest.tscn")
