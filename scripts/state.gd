ec7c3a75b514893d463d150369a4b3f5c638f98e
alvin <alvinchau2013@gmail.com>
2022-10-26 11:12:45 -0400

    added starting structure for statemachine

---
+++ scripts/state.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name State
+
+# Reference to the state machine, to call its `transition_to()` method directly.
+# The state machine node will set it.
+var state_machine = null
+
+
+# Receives events from the `_unhandled_input()` callback.
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+
+# Corresponds to the `_process()` callback.
+func update(_delta: float) -> void:
+	pass
+
+
+# Corresponds to the `_physics_process()` callback.
+func physics_update(_delta: float) -> void:
+	pass
+
+
+#  Called by the state machine upon changing the active state. The `msg` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(_msg := {}) -> void:
+	pass
+
+
+#  Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+	pass
