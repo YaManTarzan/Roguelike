90d905a08771bb4ab5736ad01bfa33a3d65d8158
alvin <alvinchau2013@gmail.com>
2022-10-27 13:59:56 -0400

    RL-4 updated paremeter naming for target state name and added explicit typing

--- scripts/states/StateMachine.gd
+++ scripts/states/StateMachine.gd
@@ -7,10 +7,10 @@ extends Node
 signal transitioned(state_name)
 
 # Path to the initial active state. We export it to be able to pick the initial state in the inspector.
-export var initial_state : NodePath = NodePath()
+export var initial_state_path_name : NodePath = NodePath()
 
 # The current active state. At the start of the game, we get the `initial_state`.
-onready var state: BaseState = get_node(initial_state)
+onready var state: BaseState = get_node(initial_state_path_name)
 
 
 func _ready() -> void:
@@ -38,14 +38,14 @@ func _physics_process(delta: float) -> void:
 # This function calls the current state's exit() function, then changes the active state,
 # and calls its enter function.
 # It optionally takes a `msg` dictionary to pass to the next state's enter() function.
-func transition_to(target_state_name: String, msg: Dictionary = {}) -> void:
+func transition_to(target_state_path_name: String, msg: Dictionary = {}) -> void:
 	# Safety check, you could use an assert() here to report an error if the state name is incorrect.
 	# We don't use an assert here to help with code reuse. If you reuse a state in different state machines
 	# but you don't want them all, they won't be able to transition to states that aren't in the scene tree.
-	if not has_node(target_state_name):
+	if not has_node(target_state_path_name):
 		return
 
 	state.exit()
-	state = get_node(target_state_name)
+	state = get_node(target_state_path_name)
 	state.enter(msg)
 	emit_signal("transitioned", state.name)
