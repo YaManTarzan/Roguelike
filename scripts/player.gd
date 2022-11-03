ef8bd3592bc7b471bcc169f8076394d74641698c
Facilitated <47868851+FacilitatedIsTaken@users.noreply.github.com>
2022-11-03 15:47:49 -0400

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,25 +3,25 @@ var velocity : Vector2 = Vector2(0,0)
 var up : Vector2 = Vector2(0,-1)
 var health : float = 100.0
 
-var walkspeed = 150
-var jumpstrength = 150
+export var walkSpeed = 150
+export var jumpStrength = 150
 
 
 func _physics_process(_delta):
 		
 	if Input.is_action_pressed("right"):
-		velocity.x = walkspeed
+		velocity.x = walkSpeed
 		$AnimatedSprite.set_flip_h(false)
 		$AnimatedSprite.set_animation('walk')
 	elif Input.is_action_pressed("left"):
-		velocity.x = -walkspeed
+		velocity.x = -walkSpeed
 		$AnimatedSprite.set_flip_h(true)
 		$AnimatedSprite.set_animation('walk')
 	else:
 		velocity.x = 0
 		$AnimatedSprite.set_animation('idle')
 	if Input.is_action_pressed('up') and is_on_floor() == true:
-		velocity.y = -jumpstrength
+		velocity.y = -jumpStrength
 	elif is_on_floor() == false:
 		velocity.y += 10
 		$AnimatedSprite.set_animation('jump')
