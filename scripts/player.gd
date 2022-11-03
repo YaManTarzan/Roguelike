21545a8774aa94e74a69cc17a1a79b970b5a75a7
ChippedObelisk <josephadams122@hotmail.com>
2022-11-03 14:50:56 -0400

    General Changes
    added variables in the player script for better editing

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,22 +3,25 @@ var velocity : Vector2 = Vector2(0,0)
 var up : Vector2 = Vector2(0,-1)
 var health : float = 100.0
 
+var walkspeed = 150
+var jumpstrength = 150
+
 
 func _physics_process(_delta):
 		
 	if Input.is_action_pressed("right"):
-		velocity.x = 200
+		velocity.x = walkspeed
 		$AnimatedSprite.set_flip_h(false)
 		$AnimatedSprite.set_animation('walk')
 	elif Input.is_action_pressed("left"):
-		velocity.x = -200
+		velocity.x = -walkspeed
 		$AnimatedSprite.set_flip_h(true)
 		$AnimatedSprite.set_animation('walk')
 	else:
 		velocity.x = 0
 		$AnimatedSprite.set_animation('idle')
 	if Input.is_action_pressed('up') and is_on_floor() == true:
-		velocity.y = -300
+		velocity.y = -jumpstrength
 	elif is_on_floor() == false:
 		velocity.y += 10
 		$AnimatedSprite.set_animation('jump')
