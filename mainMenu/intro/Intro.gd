07dd3822b2198ed5c3df552d35295f56347882e3
alvin <alvinchau2013@gmail.com>
2022-11-06 18:17:34 -0500

    RL-34 restructured project to be more organized

--- mainMenu/intro/Intro.gd
+++ mainMenu/intro/Intro.gd
@@ -8,4 +8,4 @@ func _ready():
 	yield(get_tree().create_timer(6), "timeout")
 	$AnimationPlayer.play("FadeOut")
 	yield(get_tree().create_timer(6), "timeout")
-	get_tree().change_scene("res://scenes/MainMenu.tscn")
+	get_tree().change_scene("res://mainMenu/MainMenu.tscn")
