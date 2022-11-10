6ca76b235ba4433502dac275f9df53dd4eb6804c
alvin <alvinchau2013@gmail.com>
2022-11-10 13:06:24 -0500

    fixed basic player movement logic

--- player/states/Air.gd
+++ player/states/Air.gd
@@ -3,16 +3,16 @@ extends 'State.gd'
 export var walkSpeed = 150
 export var jumpStrength = 150
 
-func _ready():
-	pass 
+func enter(_msg:={}):
+	player.velocity.y = -1200
 	
 func physics_update(_delta: float):
 	if Input.is_action_pressed("right"):
 		player.velocity.x = walkSpeed
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		player.velocity.x = -walkSpeed
-		player.velocity.y += player.gravity * _delta
-		player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
+	player.velocity.y += player.gravity * _delta
+	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
 	if player.is_on_floor():
 		if is_equal_approx(player.velocity.x,0.0):
 			state_machine.transition_to('Idle')
