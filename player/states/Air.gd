4bfedb30859b02a96221b51f38f5badbc8059be1
alvin <alvinchau2013@gmail.com>
2022-11-15 22:12:50 -0500

    fixed bug where player jumps when falls off platform

--- player/states/Air.gd
+++ player/states/Air.gd
@@ -3,14 +3,15 @@ extends 'State.gd'
 export var walkSpeed = 150
 export var jumpStrength = 150
 
-func enter(_msg:={}):
-	player.velocity.y = -1200
+func enter(msg:={}):
+	if msg.has("do_jump"):
+		player.velocity.y = -1200
 	
 func physics_update(_delta: float):
 	if Input.is_action_pressed("right"):
-		player.velocity.x = walkSpeed
+		player.velocity.x = walkSpeed * 2
 	elif Input.is_action_pressed("left"):
-		player.velocity.x = -walkSpeed
+		player.velocity.x = -walkSpeed * 2
 	player.velocity.y += player.gravity * _delta
 	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
 	if player.is_on_floor():
