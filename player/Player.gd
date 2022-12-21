33e39537bb2108b43ed505fdb474a6308214543a
alvin <alvinchau2013@gmail.com>
2022-12-20 22:47:25 -0500

    RL-39 implemented jump velocity equations

--- player/Player.gd
+++ player/Player.gd
@@ -4,6 +4,13 @@ var velocity
 var health : float = 100.0
 
 export var speed = 300
-export var jumpStrength = 150
+export var jumpHeight : float = 100
+export var jumpTimeToPeak : float = 0
+export var jumpTimeToDescent : float = 0 # time it takes to get to the bottom after jumping
+onready var jumpGravity : float = -1.0 * ((-2.0 * jumpHeight) / (jumpTimeToPeak * jumpTimeToPeak))
+onready var jumpVelocity : float = -1.0 * ((2.0 * jumpHeight) / jumpTimeToPeak)
+onready var fallGravity : float = -1.0 * ((-2.0 * jumpHeight) / (jumpTimeToDescent * jumpTimeToDescent))
 
-export var gravity = 3000.0
+
+func getGravity() -> float:
+  return jumpGravity if velocity.y < 0.0 else fallGravity
