2fbaf45869a49d514752a9795bd4b0f3447814af
alvin <alvinchau2013@gmail.com>
2022-12-24 15:13:43 -0500

    RL-39 updated explanation in comments

--- player/states/Air.gd
+++ player/states/Air.gd
@@ -9,7 +9,7 @@ export var jumpStrength = 150
 #Verlet Integration
 # https://www.algorithm-archive.org/contents/verlet_integration/verlet_integration.html
 
-# Purpose of this approach is to simply tweak certain parameters, to better adjust to the type of jump we want
+# Purpose of this approach is to simply tweak certain parameters, to better adjust to the type of jump we want to start
 # By integrating twice, f''(t) = g, you will get the position function is  1/2*g*t^2 + v0*t + p0
 # Let t(h) be the time it took to reach max height of jump, Given that f'(t) = g*t +v0, which represents velocity,
 # This would mean f'(t(h)) = 0, thus v0 = -g*t(h) which represents the initial velocity of the jump in y direction
@@ -27,7 +27,12 @@ export var jumpStrength = 150
 # g = (-2*h*v(x)^2) / x(h)^2
 
 # We will be using velocity verlet integration over euler integration because euler integration(method) can become inaccurate if the time it took to change from one frame to the next is fairly large.
-# Velocity verlet will: previous velocity + previous velocity + (gravity*timeStep) 
+# Euler method is : new velocity = current velocity + (rateOfChange) * stepSize, smaller the step size the smaller the rectangles you're making under the curve for integration
+# Verlet integration is essentially a solution to the kinematic equation of any object
+# https://www.algorithm-archive.org/contents/verlet_integration/verlet_integration.html
+# https://www.youtube.com/watch?v=AZ8IGOHsjBk
+# which ends up being simplified down to this form for numerical integration: 2*x(t) - x(t-deltaT) + a(t)deltaT^2
+# In code this tranlastes to: position = 2*position -previousPosition + acceleration * changeInTime ^2 
 
 
 func enter(msg:={}):
@@ -39,9 +44,15 @@ func physics_update(_delta: float):
 		player.velocity.x = walkSpeed * 2
 	elif Input.is_action_pressed("left"):
 		player.velocity.x = -walkSpeed * 2
-	print("TEST",player.getGravity())
-	player.velocity.y += player.getGravity() * _delta
-	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
+	var initialVelocity = player.velocity.y
+	var initialPosition = player.position.y
+
+	var finalVelocity = initialVelocity + player.getGravity() * _delta # final velocity using Verlet
+	var finalPosition = initialPosition + (finalVelocity) * (_delta*_delta) / 2 # final position using Verlet
+	player.velocity.y = finalVelocity # update velocity
+	player.position.y = finalPosition # update position
+	player.move_and_slide(player.velocity, Vector2.UP) # apply movement
+
 	if player.is_on_floor():
 		if is_equal_approx(player.velocity.x,0.0):
 			state_machine.transition_to('Idle')
